255c85ea1ca6be7b4180cc86c5f111e1fdcaf098
Rabi Uddin <rabiuddin1@gmail.com>
2024-06-15 18:10:59 +0500

    Completed Footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className="app__newsletter-heading">
+      <SubHeading title = "Newsletter"/>
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And Never Miss Latest Update</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder='Enter your email address' />
+      <button type='button' className='custom__button'>Subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React from "react";
+import { FiFacebook, FiX, FiInstagram } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
 
-import './Footer.css';
+import "./Footer.css";
+import { images } from "../../constants";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">+1 212-233-4231</p>
+        <p className="p__opensans">+1 222-643-4212</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="logo" />
+        <p className="p__opensans">
+          "The best to find your self is to lose yourself in the service of
+          others."
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiX />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday - Friday:</p>
+        <p className="p__opensans">10:00 am - 02:00 am</p>
+        <p className="p__opensans">Monday - Friday:</p>
+        <p className="p__opensans">10:00 am - 01:00 am</p>
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2024 Gericht. All Rights Reserved.</p>
+    </div>
   </div>
 );
 
